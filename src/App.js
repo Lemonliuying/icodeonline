baba488e5a9730e2cb4668391f8942a058d809d7
JasonX <lzb_sun@163.com>
2023-09-20 14:21:47 +0800

    feat: add route

--- src/App.js
+++ src/App.js
@@ -1,30 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react';
-import './App.css';
+import React from "react";
+import ReactDOM from "react";
+import "./App.css";
 
-import { Navbar, CTA } from './components';
-import { Features, Selects, Footer, Tabs } from './containers';
-import Header from './containers/header/Header';
+import { Navbar, CTA } from "./components";
+import { Features, Selects, Footer, Tabs } from "./containers";
+import Header from "./containers/header/Header";
 
-import { BrowserRouter as Router } from 'react-router-dom'; // 导入 BrowserRouter
+import { BrowserRouter as Router } from "react-router-dom"; // 导入 BrowserRouter
+import Login from "./components/login/Login.jsx";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
 
+const Root = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Features />
+      <CTA />
+      <Selects />
+      <Tabs />
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "/Login",
+    element: <Login />,
+  },
+]);
 
 const App = () => {
   return (
-    <Router>
-      <div className='App'>
-          <div className='gradient__bg'>
-              <Navbar />
-              <Header />
-          </div>
-          <Features />
-          <CTA />
-          <Selects />
-          <Tabs />
-          <Footer />
-      </div>
-    </Router>
-  )
-}
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+};
 
-export default App
+export default App;
